7483a1525ef536b6cbfdfdfe1db9d062c2878f8f
Mihai Iancu <miancu@adobe.com>
2016-01-22 00:21:40 +0200

    sample on how to post => to trigger a push

--- iamhere/ViewController.m
+++ iamhere/ViewController.m
@@ -6,6 +6,8 @@
 //  Copyright © 2016 Mobile Academy. All rights reserved.
 //
 
+#import <AFNetworking/AFNetworking.h>
+
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -22,6 +24,24 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction) pushSomething:(id)sender {
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.responseSerializer = [AFJSONResponseSerializer serializer];
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    [manager.requestSerializer setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    
+    NSString *token = @"TBD-01234";
+    NSString *msg = @"hello";
+    NSDictionary *params = @{@"token": token, @"msg": msg};
+    
+    [manager POST:@"http://iamhere2.herokuapp.com/messages" parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"JSON: %@", responseObject);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"Error: %@", operation.responseString);
+        NSLog(@"Error: %@", error);
+    }];
 }
 
 @end
