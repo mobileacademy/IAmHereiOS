c95927748b8c36c1d87a9bbb10f474f3a752b69f
Mihai Iancu <miancu@adobe.com>
2016-01-21 22:58:08 +0200

    Initial Commit

---
+++ iamhere/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iamhere
+//
+//  Created by Mihai Iancu on 21/01/16.
+//  Copyright © 2016 Mobile Academy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iamhere/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iamhere
+//
+//  Created by Mihai Iancu on 21/01/16.
+//  Copyright © 2016 Mobile Academy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
