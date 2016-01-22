a477835423710ffc3fb0f573baaade5cf283d282
Radu Dita <hades2510@yahoo.com>
2016-01-22 13:34:58 +0200

    added core location/ getting user permissions/location

--- iamhere/LocationControllerView.swift
+++ iamhere/LocationControllerView.swift
@@ -7,4 +7,56 @@
 //
 
 import Foundation
+import UIKit
+import CoreLocation
 
+class LocationControllerView: UIViewController, CLLocationManagerDelegate{
+    
+    var locationManager:CLLocationManager?
+    
+    override func viewDidLoad(){
+        
+        locationManager = CLLocationManager();
+        locationManager?.delegate = self
+        
+        switch (CLLocationManager.authorizationStatus()){
+        case .AuthorizedAlways:
+                break;
+        case .AuthorizedWhenInUse:
+            requestLocation()
+            break;
+        case .Denied:
+            break;
+        case .NotDetermined:
+                self.requestAuth()
+            break;
+        case .Restricted:
+            break;
+        }
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        locationManager?.stopUpdatingLocation()
+    }
+    
+    func requestAuth(){
+        locationManager?.requestWhenInUseAuthorization()
+    }
+    
+    func requestLocation(){
+        locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager?.startUpdatingLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+       requestLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let finalEl = locations[locations.count-1]
+        
+        print( finalEl )
+    }
+    
+}
